4a2c15c9936415359947f4308442af6aaf0828cc
adparnel <adparnel@asu.edu>
2024-10-28 09:45:07 -0700

    Update Autonomous.m

--- Autonomous.m
+++ Autonomous.m
@@ -1,13 +1,29 @@
-brick = ConnectBrick('TEN');
+brick = ConnectBrick('ANT');
 done = 0;
-
+% brick.setColorMode(2,1); % port 2, ambient light mode
+% Color sensor must be connected t port 2, ultrasonic to D, 
+% touch sensor to A, 
+% Motors are B and C.
 while done == 0
-    distance = brick.UltrasonicDist(2);
-    while brick.TouchPressed(1) == 0
+    % color = brick.ColorCode(2);
+    distance = brick.UltrasonicDist("4");
+    while brick.TouchPressed("3") == 0
         brick.MoveMotor('B', 40);
         brick.MoveMotor('C', 40);
+        %if color == 5
+            % add code for red
+        %end
+        %if color == 3
+            % add code for green
+        %end
+        %if color == 2
+            % add cpde for blue
+        %end
+        %if color == 4
+            % add code for yellow
+        %end
     end
-    if brick.TouchPressed(1) == 1
+    if brick.TouchPressed("A") == 1
         brick.StopAllMotors('Brake');
         brick.MoveMotor('B', -30);
         brick.MoveMotor('C', -30);
@@ -21,9 +37,6 @@ while done == 0
             brick.MoveMotor('C', -10);
             pause(3.7);
         end
-
-
         
     end 
-
 end 
